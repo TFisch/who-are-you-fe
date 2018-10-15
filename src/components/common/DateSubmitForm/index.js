304b09c7b54fee2eae6b843bff15dbf3d91ab79e
Tim Fischer <emailtimfischer@gmail.com>
2018-10-15 15:01:15 -0600

    Add DateSubmitForm boilerplate

---
+++ src/components/common/DateSubmitForm/index.js
@@ -0,0 +1,16 @@
+import react, { Component } from 'react';
+
+class DateSubmitForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      null
+    )
+  }
+}
