3324b2f203a18cf8ad1dd4335edf45967cc3f280
Tim Fischer <emailtimfischer@gmail.com>
2018-10-15 15:33:15 -0600

    Dynamically save values of text and date input

--- src/components/common/DateSubmitForm/index.js
+++ src/components/common/DateSubmitForm/index.js
@@ -4,8 +4,14 @@ class DateSubmitForm extends Component {
   constructor() {
     super()
     this.state = {
-
+      nameInput: "",
+      dateInput: 0
     }
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value })
   }
 
   render() {
@@ -13,9 +19,9 @@ class DateSubmitForm extends Component {
       <div className="dateSubmitForm">
         <h1>{this.props.headerText}</h1>
         <h3>{this.props.inputOneText}</h3>
-        <input type="text" />
+        <input type="text" name="nameInput" onChange={this.handleChange} value={this.nameInput} />
         <h3>{this.props.inputTwoText}</h3>
-        <input type="date" />
+        <input type="date" name="dateInput" onChange={this.handleChange} value={this.dateInput} />
       </div>
     )
   }
