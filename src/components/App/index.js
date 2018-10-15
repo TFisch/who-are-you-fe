7ac365ef6ccf2cc52e76a022db925961ad0de3b7
Tim Fischer <emailtimfischer@gmail.com>
2018-10-15 17:29:02 -0600

    Add heroku webpack

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import DateSubmitForm from '../common/DateSubmitForm'
 import './App.css';
+import { fetchDeaths } from '../../utilities/apiCalls'
 
 class App extends Component {
   constructor() {
     super()
+  }
+
+  componentDidMount() {
+    fetchDeaths();
   }
 
 
