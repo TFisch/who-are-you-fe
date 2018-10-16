52e923ed08bfafb17d588fc877b78ae2753c9966
Tim Fischer <emailtimfischer@gmail.com>
2018-10-16 16:25:24 -0600

    Render component

--- src/components/ReincarnationDisplay/index.js
+++ src/components/ReincarnationDisplay/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Submit from '../Submit';
+
 
 class ReincarnationDisplay extends Component {
   constructor() {
@@ -6,6 +8,20 @@ class ReincarnationDisplay extends Component {
     state = {
 
     }
+  }
+
+  handleSubmit = (e) => {
+    console.log('hi')
+  }
+
+  render() {
+    return (
+      <article className="reincarnation-display">
+        <h2>Name! You Were DEAD PERSON in Your Past Life!</h2>
+
+        <Submit handleSubmit={this.handleSubmit} />
+      </article>
+    )
   }
 }
 
