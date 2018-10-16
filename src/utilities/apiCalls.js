d6ad1474ff4412f70f519c26609e03c5e2cbdbac
Tim Fischer <emailtimfischer@gmail.com>
2018-10-16 13:11:39 -0600

    Add  extra slash on file path url

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -1,5 +1,5 @@
 export const fetchDeaths = async () => {
-  const url = process.env.REACT_APP_DATABASE_API_URL + 'api/v1/deaths';
+  const url = process.env.REACT_APP_DATABASE_API_URL + '/api/v1/deaths';
   const response = await fetch(url);
   const data = await response.json();
   await console.log(data);
