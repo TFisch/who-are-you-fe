271c5a07a2bcd76425610a8c64e73f036c9d30f2
Tim Fischer <emailtimfischer@gmail.com>
2018-10-16 17:22:34 -0600

    Wrap comment section

--- src/components/ReincarnationDisplay/index.js
+++ src/components/ReincarnationDisplay/index.js
@@ -19,8 +19,14 @@ class ReincarnationDisplay extends Component {
     return (
       <article className="reincarnation-display">
         <h2>Name! You Were DEAD PERSON in Your Past Life!</h2>
-
-        <Submit buttonText="SAVE" handleSubmit={this.handleSubmit} />
+        <div className="comment-wrapper">
+          <label for="comment">Leave a comment:</label>
+          <div className="bottom-wrapper">
+            <textarea class="comment" name="comment">
+            </textarea>
+            <Submit buttonText="SAVE" handleSubmit={this.handleSubmit} />
+          </div>
+        </div>
       </article>
     )
   }
