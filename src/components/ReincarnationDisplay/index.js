ca86d9c6d3f6f6a40b6018254b4a8baf4c658f0f
Tim Fischer <emailtimfischer@gmail.com>
2018-10-17 13:16:15 -0600

    Add death name from props

--- src/components/ReincarnationDisplay/index.js
+++ src/components/ReincarnationDisplay/index.js
@@ -7,18 +7,24 @@ class ReincarnationDisplay extends Component {
   constructor() {
     super()
     this.state = {
-
+      deathMatch: {}
     }
   }
+
+  componentDidMount = () => {
+    this.setState({ deathMatch: this.props.deathsByDate })
+  }
+
 
   handleSubmit = (e) => {
     console.log(e);
   }
 
   render() {
+    const name = Object.values(this.props.deathsByDate[0]);
     return (
       <article className="reincarnation-display">
-        <h2>{this.props.username} You Were DEAD PERSON in Your Past Life!</h2>
+        <h2>{this.props.username}! You Were {name[1]} In Your Past Life!</h2>
         <div className="comment-wrapper">
           <label>Leave a comment:</label>
           <div className="bottom-wrapper">
