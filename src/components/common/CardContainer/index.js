cba3fed1f54f6f5b24e5a4afa8bf5b3de8221aac
Cody Taft <cody.taft@gmail.com>
2018-10-17 16:48:54 -0600

    Add title header bar to card container

--- src/components/common/CardContainer/index.js
+++ src/components/common/CardContainer/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReincarnatedUser from '../../ReincarnatedUser';
+import './styles.css';
 
 const CardContainer = ({ users, handleClick }) => {
   const displayUsers = users.map(user => {
     return <ReincarnatedUser user={user} handleClick={handleClick} />;
   });
-  return <div className="card-container">{displayUsers}</div>;
+  return (
+    <div className="card-container">
+      <div className="table-headers">
+        <h3 className="user-name">User Name</h3>
+        <h3 className="reincarnated-person">Reincarnated As</h3>
+        <p className="day">Birthday</p>
+        <p className="year">Year Born</p>
+        <p className="astrology-sign">Astrological Sign</p>
+        <p className="delete-title">Delete?</p>
+      </div>
+      {displayUsers}
+    </div>
+  );
 };
 
 export default CardContainer;
