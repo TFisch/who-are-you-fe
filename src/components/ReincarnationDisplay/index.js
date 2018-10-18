9d0ba19da3fbb3b0ed3b256259936fa31f7ff70f
Cody Taft <cody.taft@gmail.com>
2018-10-17 21:02:50 -0600

    Add error message if no dead people present

--- src/components/ReincarnationDisplay/index.js
+++ src/components/ReincarnationDisplay/index.js
@@ -3,46 +3,59 @@ import Submit from '../common/Submit';
 import { postUsers } from '../../utilities/apiCalls';
 import './styles.scss';
 
-
 class ReincarnationDisplay extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       deathMatch: {},
-      notes: ""
-    }
+      notes: ''
+    };
   }
 
   componentDidMount = () => {
-    this.setState({ deathMatch: this.props.deathsByDate })
-  }
+    this.setState({ deathMatch: this.props.deathsByDate });
+  };
 
-  handleNotes = (e) => {
-    this.setState({ notes: e.target.value })
-  }
+  handleNotes = e => {
+    this.setState({ notes: e.target.value });
+  };
 
-
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     postUsers(this.props.username, this.props.deathsByDate, this.state.notes);
-    this.setState({ notes: "" });
+    this.setState({ notes: '' });
     //s
-  }
+  };
 
   render() {
-    const name = Object.values(this.props.deathsByDate[0]);
-    return (
-      <article className="reincarnation-display">
-        <h2>{this.props.username}! You Were {name[1]} In Your Past Life!</h2>
-        <div className="comment-wrapper">
-          <label>Leave a comment:</label>
-          <div className="bottom-wrapper">
-            <textarea className="comment" name="comment" onChange={this.handleNotes} value={this.state.notes}>
-            </textarea>
-            <Submit buttonText="SAVE" handleSubmit={this.handleSubmit} />
+    if (this.props.deathsByDate.length) {
+      var name = Object.values(this.props.deathsByDate[0]);
+      return (
+        <article className="reincarnation-display">
+          <h2>
+            {this.props.username}! You Were {name[1]} In Your Past Life!
+          </h2>
+          <div className="comment-wrapper">
+            <label>Leave a comment:</label>
+            <div className="bottom-wrapper">
+              <textarea
+                className="comment"
+                name="comment"
+                onChange={this.handleNotes}
+                value={this.state.notes}
+              />
+              <Submit buttonText="SAVE" handleSubmit={this.handleSubmit} />
+            </div>
           </div>
-        </div>
-      </article>
-    )
+        </article>
+      );
+    } else {
+      return (
+        <article className="reincarnation-display">
+          <h2>Sorry no soul wanted to embody your body...</h2>
+          <button onClick={this.props.tryAgain}>Try Again?</button>
+        </article>
+      );
+    }
   }
 }
 
