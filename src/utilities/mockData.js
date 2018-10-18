6f6b70d1f303c7fde87dbe3f83994a12f87b15f7
Tim Fischer <emailtimfischer@gmail.com>
2018-10-17 21:30:20 -0600

    mock  deathsByDate for snapshot

---
+++ src/utilities/mockData.js
@@ -0,0 +1,9 @@
+export const mockDeath = [{
+  created_at: "2018-10-13T00:30:37.227Z",
+  date_id: 414,
+  deletable: false,
+  id: 1362,
+  person_name: "Lefty Gomez",
+  updated_at: "2018-10-13T00:30:37.227Z",
+  year: 1989
+}]
