a339f840b6587a91242040f53e5120149d433f29
Tim Fischer <emailtimfischer@gmail.com>
2018-10-17 21:36:01 -0600

    Add CardContainer snapshot

--- src/utilities/mockData.js
+++ src/utilities/mockData.js
@@ -7,3 +7,13 @@ export const mockDeath = [{
   updated_at: "2018-10-13T00:30:37.227Z",
   year: 1989
 }]
+
+export const mockUsers = [{
+  astrologySign: "Aquarius",
+  day: "FEBRUARY 17",
+  id: 6,
+  name: "Cody Taft",
+  notes: "Well hello there",
+  reincarnatedPerson: "Lefty Gomez",
+  year: 1989
+}]
